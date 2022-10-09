4de9e72e355f75c7ea024e86dc4df7b53953d888
Bart Duisters <bartduisters@bartduisters.com>
2022-10-09 13:59:54 +0200

    feat: oef 2

---
+++ oef-2/Program.cs
@@ -0,0 +1,24 @@
+﻿for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"i is momenteel {i}");
+}
+
+// Herschrijf bovenstaande code met een while-loop
+// Gebruik `j` i.p.v. `i`
+int j = 0;
+while (j < 3)
+{
+    Console.WriteLine($"j is momenteel {j}");
+    j++;
+}
+
+/*
+* Wanneer `dotnet run Program.cs` wordt uitgevoerd, moet de volgende output verschijnen:
+*/
+
+// i is momenteel 0
+// i is momenteel 1
+// i is momenteel 2
+// j is momenteel 0
+// j is momenteel 1
+// j is momenteel 2
